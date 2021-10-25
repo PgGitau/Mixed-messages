c4557220864ecaca18e75dde13bad07c03ef2cd2
r3t3p <92801240+PgGitau@users.noreply.github.com>
2021-10-25 15:59:15 +0300

    a working message generator

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let randNum = Math.floor(Math.random() * 4)
-console.log(`The random Number is ${randNum}`)
+console.log(`Your random Number is ${randNum}`)
 
 const charactersFactory = (names, tribe, bends) => {
     return {
@@ -10,11 +10,23 @@ const charactersFactory = (names, tribe, bends) => {
 }
 const characters1 = charactersFactory('Aang', 'Air Nation', "Everything(HE'S THE AVATAR!!)");
 const characters2 = charactersFactory('Katara', 'Water Nation', "Water(SHE'S THE PRINCESS!!)");
-const characters3 = charactersFactory('Sokka', 'Water Nation', "Nothing(BUT HE'S THE WARRIOR & JOKER TOO!!)");
+const characters3 = charactersFactory('Sokka', 'Water Nation', "Nothing(BUT HE'S THE WARRIOR & FUNNY TOO!!)");
 const characters4 = charactersFactory('Zuko', 'Fire Nation', "Fire(HE'S THE GREAT EMPEROR!!)");
-console.log(characters2)
-/*if (randNum === 0){
-    'your character is ';
-    "you're from the ";
-    'you bend ';
-}*/
+
+if (randNum === 0){
+    console.log(`It's character is ${characters1.names}.`) 
+    console.log(`He's from the ${characters1.tribe}.`) 
+    console.log(`He bends ${characters1.bends}.`) 
+}else if (randNum === 1){
+    console.log(`It's character is ${characters2.names}`)
+    console.log(`He's from the ${characters2.tribe}.`) 
+    console.log(`He bends ${characters2.bends}.`) 
+}else if (randNum === 2){
+    console.log(`It's character is ${characters3.names}`)
+    console.log(`He's from the ${characters3.tribe}.`) 
+    console.log(`He bends ${characters3.bends}.`) 
+}else {
+    console.log(`It's character is ${characters4.names}`)
+    console.log(`He's from the ${characters4.tribe}.`) 
+    console.log(`He bends ${characters4.bends}.`) 
+}
